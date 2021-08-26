e305f3fede1c12363f08701f422d959f48e5df5f
hhhira326 <ry000xx@gmail.com>
2021-08-26 22:21:40 +0900

    Add user microposts

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -24,10 +24,6 @@ class MicropostsController < ApplicationController
       params.require(:micropost).permit(:content, :picture)
     end
 
-    def micropost_params
-      params.require(:micropost).permit(:content)
-    end
-
     def correct_user
       @micropost = current_user.microposts.find_by(id: params[:id])
       redirect_to root_url if @micropost.nil?
